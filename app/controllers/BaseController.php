6b5dc6cce799b637253dd2a6d87ae48e6bb0242f
Kennon Bickhart <kennon@kennonb.me>
2014-05-30 16:21:42 -0400

    Testing an output issue when Controller based layouts are used.
    Signed-off-by: Kennon Bickhart <kennon@kennonb.me>

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,17 +2,35 @@
 
 class BaseController extends Controller {
 
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-		}
-	}
+    protected $layout = 'layouts.default';
+
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    protected function setupLayout()
+    {
+        if ( ! is_null($this->layout))
+        {
+            $this->layout = View::make($this->layout);
+        }
+    }
+
+    public function source()
+    {
+        return DataGrid::make(new City, array(
+            'id',
+            'country',
+            'subdivision',
+            'city',
+            'population',
+            'created_at',
+        ), array(
+            'sort' => 'id',
+            'direction' => 'desc',
+            'max_results' => 20,
+        ));
+    }
 
 }
